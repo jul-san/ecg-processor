ff35509a40113bd73a1b05a37e8741e752de3585
Julian Sanchez <julianwsan@gmail.com>
2024-11-15 17:10:42 -0500

    ☝️ Initial file

---
+++ separator/main.py
@@ -0,0 +1,7 @@
+print("\nSuperimposed Electrocardiographic (ECG) Separator")
+print("-------------------------------------------------")
+print("To use your own data set, place the ECG recording\ninside of the data directory. The file must be a CSV\nand has to be formatted using the following structure:\n\nColumn 1  | Column 2\n-------------------------\nMixed ECG | Caregiver ECG")
+
+
+
+file = input("\n\nEnter the name of the data set: ")
